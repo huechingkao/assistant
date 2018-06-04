4333d8fdc086410cb21c2ba8076ffe29104fc04a
idreamer <idreamer@gmail.com>
2018-06-04 05:44:09 -0400

    Beautify page templates

--- web/views.py
+++ web/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Diary
-from .models import Money
+from .models import Diary, Money
+from .forms import DiaryForm, MoneyForm
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
@@ -10,50 +10,48 @@ from django.utils.decorators import method_decorator
 class DiaryListView(generic.ListView):
     model = Diary
     ordering = ['-id']
-    paginate_by = 3  
+    paginate_by = 3
 
-@method_decorator(login_required, name='dispatch')    
+@method_decorator(login_required, name='dispatch')
 class DiaryCreate(CreateView):
-    model = Diary
-    fields = ['memo']
-    success_url = "/"   
-    template_name = 'form.html'   
+    form_class = DiaryForm
+    success_url = "/"
+    template_name = 'form.html'
 
-@method_decorator(login_required, name='dispatch')    
+@method_decorator(login_required, name='dispatch')
 class DiaryUpdate(UpdateView):
     model = Diary
-    fields = '__all__'   
-    success_url = "/"   
-    template_name = 'form.html'    
+    form_class = DiaryForm
+    success_url = "/"
+    template_name = 'form.html'
 
-@method_decorator(login_required, name='dispatch')    
+@method_decorator(login_required, name='dispatch')
 class DiaryDelete(DeleteView):
     model = Diary
     success_url = "/"
-    template_name = 'confirm_delete.html' 
+    template_name = 'confirm_delete.html'
 
-@method_decorator(login_required, name='dispatch')        
+@method_decorator(login_required, name='dispatch')
 class MoneyListView(generic.ListView):
     model = Money
     ordering = ['-id']
-    paginate_by = 3  
+    paginate_by = 3
 
-@method_decorator(login_required, name='dispatch')    
+@method_decorator(login_required, name='dispatch')
 class MoneyCreate(CreateView):
-    model = Money
-    fields = '__all__'
+    form_class = MoneyForm
     success_url = "/web/money"
     template_name = 'form.html'
 
-@method_decorator(login_required, name='dispatch')    
+@method_decorator(login_required, name='dispatch')
 class MoneyUpdate(UpdateView):
     model = Money
-    fields = '__all__'
-    success_url = "/web/money"    
-    template_name = 'form.html'      
+    form_class = MoneyForm
+    success_url = "/web/money"
+    template_name = 'form.html'
 
-@method_decorator(login_required, name='dispatch')    
+@method_decorator(login_required, name='dispatch')
 class MoneyDelete(DeleteView):
     model = Money
-    success_url = "/web/money"   
-    template_name = 'confirm_delete.html'           
+    success_url = "/web/money"
+    template_name = 'confirm_delete.html'
